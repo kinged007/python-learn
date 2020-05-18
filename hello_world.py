262f614362427b68d55fdc70d476e0e02dfc9e1d
kinged007 <joshuab.za@gmail.com>
2020-05-18 22:56:58 +0200

    init

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+
+x = 1
+if x == 1 :
+    print("x is ",  x)
+
+while x is not 10:
+    print(x)
+    x += 1
+    
+print("Done!")
+
+# https://www.learnpython.org/en/Variables_and_Types
+
+one = 1
+two = 2
+three = one + two
+
+print( "one + two = ", three )
+
+hello = "hello"
+world = "world"
+
+print( hello + " " + world)
+
+a,b = 3,4
+print(a,b)
+
+print("String replacement (cant do more than one?): %s." % hello)
+
+# https://www.learnpython.org/en/Lists
+
